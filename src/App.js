748713000effbc58faec44bdfd15de9e2443aaaf
Kelum Sampath <KelumS@99x.lk>
2019-09-17 10:00:21 +0530

    status update seted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,98 @@
 import React from 'react';
 import './App.css';
-import { Table,thead,th, tr,tbody } from 'react-bootstrap';
+import { Table, thead, th, tr, tbody, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="heading">
-    <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>New Complain</th>
-      <th>case Handler</th>
-      <th>Completion</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</Table>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "New"
+    };
+
+    this.backward = this.backward.bind(this);
+    this.internal = this.internal.bind(this);
+    this.forward = this.forward.bind(this);
+  }
+
+  backward(){
+    console.log("backward")
+    var dataset={
+      mystate:this.state.status,
+      action:"backward"
+    }
+    this.TriggerApi(dataset)
+  }
+
+  internal(){
+    console.log("indter")
+    var dataset={
+      mystate:this.state.status,
+      action:"internal"
+    }
+    this.TriggerApi(dataset)
+  }
+
+  forward(){
+    console.log("forss")
+    var dataset={
+      mystate:this.state.status,
+      action:"forward"
+    }
+    this.TriggerApi(dataset)
+  }
+
+  TriggerApi(dataset){
+    console.log(dataset)
+    
+  }
+
+  render() {
+    return (
+      <div className="heading">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>New Complain</th>
+              <th>case Handler</th>
+              <th>Completion</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Card className="card">
+                  <h3>Complain</h3>
+                  <p>A text can be any example of written or spoken language, from something as complex as a book or legal document to something as simple as the body of an email or the words on the back of a cereal box.</p>
+                  <h4>Status:  {this.state.status}</h4>
+                  <div>
+                    <button onClick={this.backward} >backward</button>
+                    <button onClick={this.internal}>Internal</button>
+                    <button onClick={this.forward}>Forward</button>
+                  </div>
+                </Card>
+
+              </td>
+              <td>
+                <Card className="card">
+                  <h3>Complain</h3>
+                  <p>A text can be any example of written or spoken language, from something as complex as a book or legal document to something as simple as the body of an email or the words on the back of a cereal box.</p>
+                </Card>
+              </td>
+              <td>
+                <Card className="card">
+                  <h3>Complain</h3>
+                  <p>A text can be any example of written or spoken language, from something as complex as a book or legal document to something as simple as the body of an email or the words on the back of a cereal box.</p>
+                </Card>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
+
 
 export default App;
